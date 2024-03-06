334cc36ccab173fd3ca9bc222ef15e20e3716f13
cdottori <caio.dottori@starkbank.com>
2024-03-06 19:56:04 -0300

    Bump to v0.2.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -13,6 +13,8 @@ Given a version number MAJOR.MINOR.PATCH, increment:
 
 
 ## [Unreleased]
+
+## [0.2.1] - 2024-03-06
 ## Fixed
 - PUT method reference
 
--- starkcore/__init__.py
+++ starkcore/__init__.py
@@ -4,7 +4,7 @@ if sys.version_info.major == 2:
     reload(sys)
     sys.setdefaultencoding("UTF8")
 
-version = "0.2.0"
+version = "0.2.1"
 
 from .environment import Environment
 from .user import Organization, Project, User
